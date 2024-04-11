0d6e63948f32aaee534af6e108d55f2970fff26e
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-04-11 13:50:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ICS2O Unit #X-YY
+
+[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-7f7980b617ed060a017424585567c406b6ee15c891e84e1186181d67ecf80aa0.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=14345038)
+
+[![Mr Coxall's Super Linter](https://github.com/MTHS-ICD2O-1-2023/ICD2O-Unit-1-08-PHP-clara-tyman/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/MTHS-ICD2O-1-2023/ICD2O-Unit-1-08-PHP-clara-tyman/actions)
+
+---
+
+**NOTES ON PHP:**
+- to test out your PHP files run the following command in the terminal:
+```console
+  php -S 0.0.0.0:8000
+```
+- you CAN NOT save this to GitHub Pages, since it needs a server in the backend to run it
+  - you will need to save it to the Class Server
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="XXX" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Clara Tyman" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-indigo.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link sizes="32x32" href="favicon-32x32.png" />
+  <link sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>XXX</title>
+</head>
+
+</html>
